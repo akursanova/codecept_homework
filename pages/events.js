26a5321dca9b68ea21fd370c2b29cb009f20d78b
Alexandra.Kursanova <alexandra.kursanova@nordigy.ru>
2021-07-08 18:48:49 +0300

    change results locator

--- pages/events.js
+++ pages/events.js
@@ -8,7 +8,7 @@ module.exports = {
   running: '#app > main > section > div > div > div.grid__col.grid__col_lg-auto > div.events-filters > div.events-filters__container > div.d-none.d-md-block > div:nth-child(1) > div.filter__content > div > div > div.multiselect__content-wrapper > ul > li:nth-child(15) > span',
   showButton: '.events-filters__footer > .grid > .grid__col > .rr-button_primary > .rr-button__text',
   clearButton: '#app > main > section > div > div > div.grid__col.grid__col_lg-auto > div.events-filters > div.events-filters__container > div.d-none.d-md-block > div.events-filters__footer > div > div:nth-child(1) > button',
-  resultsLink: '#app > main > section > div > div > div.grid__col.grid__col_lg_stretch > div.section__header > div.section__subheader > div > div > div.sort__wrapper.d-none.d-md-flex > div:nth-child(4) > a',
+  resultsLink: '.section__subheader [data-index = \'3\']',
 
   moreEvents() {
     I.click(this.moreEventsButton);
